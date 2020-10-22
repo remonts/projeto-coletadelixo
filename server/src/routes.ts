f8a5c88607e0f130a8b57e5114ca2ef53b5ddfa3
remonts <monteiro-renan@hotmail.com>
2020-10-22 05:39:31 -0300

    Corrigido erro na consulta ao banco da funcionalidade listar pontos específicos.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,11 +6,11 @@ import ItemsController from './controllers/ItemsController';
 const routes = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
-// Teste de commit
 
 routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index); 
 routes.get('/points/:id', pointsController.show);
 
 export default routes;
