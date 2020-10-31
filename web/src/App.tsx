0219fb051d02386a665c52248dc9fa57116da205
remonts <monteiro-renan@hotmail.com>
2020-10-31 08:58:29 -0300

    Construída primeira página da aplicação.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,12 +2,12 @@ import { homedir } from 'os';
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home'
+import Routes from './routes';
 
 function App() {
 
   return (        
-      <Home />
+      <Routes />
   );
 }
 
