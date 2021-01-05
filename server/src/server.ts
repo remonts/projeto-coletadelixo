c0aa6388a5aa2f0d1f3550630e2aa79aad43b81a
remonts <monteiro-renan@hotmail.com>
2021-01-05 10:44:25 -0300

    Projeto finalizado

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
+import { errors } from 'celebrate';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Configurando o acesso as imagens
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
+
+app.listen(3333);
